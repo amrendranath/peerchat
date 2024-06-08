35bdbccb1976d72092a13e25d3e1995cce9bc9a1
amrendranath <cool.amrendrasrivastav@gmail.com>
2024-06-08 19:40:52 +0530

    peer chat setup

---
+++ main.js
@@ -0,0 +1,244 @@
+let APP_ID = "35376878bca144c6952ffbe41112cb7e";
+
+let token = null;
+let uid = String(Math.floor(Math.random() * 10000));
+
+let client;
+let channel;
+
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let roomId = urlParams.get("room");
+
+if (!roomId) {
+  window.location = "lobby.html";
+}
+
+let localStream;
+let remoteStream;
+let peerConnection;
+
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+};
+
+let constraints = {
+  video: {
+    width: { min: 640, ideal: 1920, max: 1920 },
+    height: { min: 480, ideal: 1080, max: 1080 },
+  },
+  audio: true,
+};
+
+let init = async () => {
+  client = await AgoraRTM.createInstance(APP_ID);
+  await client.login({ uid, token });
+
+  channel = client.createChannel(roomId);
+  await channel.join();
+
+  channel.on("MemberJoined", handleUserJoined);
+  channel.on("MemberLeft", handleUserLeft);
+
+  client.on("MessageFromPeer", handleMessageFromPeer);
+
+  localStream = await navigator.mediaDevices.getUserMedia(constraints);
+  document.getElementById("user-1").srcObject = localStream;
+};
+
+let handleUserLeft = (MemberId) => {
+  document.getElementById("user-2").style.display = "none";
+  document.getElementById("user-1").classList.remove("smallFrame");
+};
+
+let handleMessageFromPeer = async (message, MemberId) => {
+  message = JSON.parse(message.text);
+
+  if (message.type === "offer") {
+    createAnswer(MemberId, message.offer);
+  }
+
+  if (message.type === "answer") {
+    addAnswer(message.answer);
+  }
+
+  if (message.type === "candidate") {
+    if (peerConnection) {
+      peerConnection.addIceCandidate(message.candidate);
+    }
+  }
+};
+
+let handleUserJoined = async (MemberId) => {
+  console.log("A new user joined the channel:", MemberId);
+  createOffer(MemberId);
+};
+
+let createPeerConnection = async (MemberId) => {
+  peerConnection = new RTCPeerConnection(servers);
+
+  remoteStream = new MediaStream();
+  document.getElementById("user-2").srcObject = remoteStream;
+  document.getElementById("user-2").style.display = "block";
+
+  document.getElementById("user-1").classList.add("smallFrame");
+
+  if (!localStream) {
+    localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+    document.getElementById("user-1").srcObject = localStream;
+  }
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  peerConnection.onicecandidate = async (event) => {
+    if (event.candidate) {
+      client.sendMessageToPeer(
+        {
+          text: JSON.stringify({
+            type: "candidate",
+            candidate: event.candidate,
+          }),
+        },
+        MemberId
+      );
+    }
+  };
+};
+
+let createOffer = async (MemberId) => {
+  await createPeerConnection(MemberId);
+
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+
+  client.sendMessageToPeer(
+    { text: JSON.stringify({ type: "offer", offer: offer }) },
+    MemberId
+  );
+};
+
+let createAnswer = async (MemberId, offer) => {
+  await createPeerConnection(MemberId);
+
+  await peerConnection.setRemoteDescription(offer);
+
+  let answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+
+  client.sendMessageToPeer(
+    { text: JSON.stringify({ type: "answer", answer: answer }) },
+    MemberId
+  );
+};
+
+let addAnswer = async (answer) => {
+  if (!peerConnection.currentRemoteDescription) {
+    peerConnection.setRemoteDescription(answer);
+  }
+};
+
+let leaveChannel = async () => {
+  await channel.leave();
+  await client.logout();
+};
+
+let toggleCamera = async () => {
+  let videoTrack = localStream
+    .getTracks()
+    .find((track) => track.kind === "video");
+
+  if (videoTrack.enabled) {
+    videoTrack.enabled = false;
+    document.getElementById("camera-btn").style.backgroundColor =
+      "rgb(255, 80, 80)";
+  } else {
+    videoTrack.enabled = true;
+    document.getElementById("camera-btn").style.backgroundColor =
+      "rgb(179, 102, 249, .9)";
+  }
+};
+
+let toggleMic = async () => {
+  let audioTrack = localStream
+    .getTracks()
+    .find((track) => track.kind === "audio");
+
+  if (audioTrack.enabled) {
+    audioTrack.enabled = false;
+    document.getElementById("mic-btn").style.backgroundColor =
+      "rgb(255, 80, 80)";
+  } else {
+    audioTrack.enabled = true;
+    document.getElementById("mic-btn").style.backgroundColor =
+      "rgb(179, 102, 249, .9)";
+  }
+};
+
+const captureImage = async () => {
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  const video = document.getElementById("user-1");
+
+  // Reverse the canvas context to create a mirror image
+  context.save();
+  context.translate(canvas.width, 0);
+  context.scale(-1, 1);
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  context.restore();
+
+  canvas.style.display = "block";
+
+  const imageDataURL = canvas.toDataURL("image/png");
+  console.log(imageDataURL);
+
+  // Create a temporary link element to trigger the download
+  const downloadLink = document.createElement("a");
+  downloadLink.href = imageDataURL;
+  downloadLink.download = "captured-image.png";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+
+  // To send API to save capture image
+  // try {
+  //   const response = await fetch("/upload", {
+  //     method: "POST",
+  //     body: JSON.stringify({ image: imageDataURL }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   const data = await response.json();
+  //   console.log("Success:", data);
+  // } catch (error) {
+  //   console.error("Error:", error);
+  // }
+
+  // Hide the canvas after a delay
+  setTimeout(() => {
+    canvas.style.display = "none";
+  }, 5000); // Hide canvas after 5 seconds
+};
+
+window.addEventListener("beforeunload", leaveChannel);
+
+document.getElementById("camera-btn").addEventListener("click", toggleCamera);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
+document.getElementById("picture-btn").addEventListener("click", captureImage);
+
+init();
